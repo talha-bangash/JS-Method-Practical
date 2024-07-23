8e49f8b575f1c0669ae8ca214b023a07c463b307
talha-bangash <tmbangash2006@gmail.com>
2024-07-23 15:11:52 +0500

    done

---
+++ external.js
@@ -0,0 +1,27 @@
+let strsps = " Hello-World";
+let str = strsps.trim();
+let str2 = str + " World";
+let arr = ["Hello","World"]
+
+
+
+
+console.log(str.trim());
+
+console.log(str.substring(5,11));
+
+console.log(str.substr(0,5));
+
+console.log(str.split("-"));
+
+console.log(arr.join(" "));
+
+console.log(str.indexOf("o"));
+
+console.log(str2.lastIndexOf("World"));
+
+console.log("World".startsWith("Wor"));
+
+console.log(str.endsWith("World"));
+
+console.log(str.includes("o"));
